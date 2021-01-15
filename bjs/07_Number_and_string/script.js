16e7e3834b27804e1ca4228f5c9541003fb0ab28
ElenaChebysheva <f9292@yandex.ru>
2021-01-15 23:38:16 +0300

    master

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -1,60 +1,71 @@
 let lastOperand = 0;
 let operation = null;
 
-// function funonload() {
-//  inputWindow.value = '0';
- 
-// } 
-
-
-// window.onload = funonload;//при загрузке страница выдает значение 0 в калькуляторе
-
-
-
 const inputWindow = document.getElementById('inputWindow');
-
+inputWindow.value = '0';
+ 
 
 document.getElementById('btn_1').addEventListener('click', function () {
+     if (inputWindow.value === '0') {
+            inputWindow.value = '';
+        }
+        inputWindow.value +='1';
     
-  inputWindow.value += '1' ;
-      
 })
 
 document.getElementById('btn_2').addEventListener('click', function () {
-    
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '2';
 })
 document.getElementById('btn_3').addEventListener('click', function () {
-    
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '3';
 })
 document.getElementById('btn_4').addEventListener('click', function () {
-    
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '4';
 })
 
 document.getElementById('btn_5').addEventListener('click', function () {
-    
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '5';
 })
 document.getElementById('btn_6').addEventListener('click', function () {
-    
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '6';
 })
 document.getElementById('btn_7').addEventListener('click', function () {
-   
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '7';
 })
 document.getElementById('btn_8').addEventListener('click', function () {
-    
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '8';
 })
 document.getElementById('btn_9').addEventListener('click', function () {
-    
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '9';
 })
 document.getElementById('btn_0').addEventListener('click', function () {
-    
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '0';
 })
 
@@ -107,7 +118,7 @@ document.getElementById('btn_unary').addEventListener('click', function () {
 document.getElementById('btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
-    inputWindow.value = ''; 
+    inputWindow.value = '0'; 
 })
 
 
@@ -115,8 +126,10 @@ document.getElementById('btn_calc').addEventListener('click', function () {
   if (operation === 'sum') {
       const result = parseFloat(lastOperand) + parseFloat(inputWindow.value);
       operation = null;
+     
       lastOperand = 0;
       inputWindow.value = result;
+
   }
   
   if (operation === 'def') {
