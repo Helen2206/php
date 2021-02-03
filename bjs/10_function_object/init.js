9c78f3a457b1bf4a4ea731048e4b77600ef0594d
ElenaChebysheva <f9292@yandex.ru>
2021-02-03 21:59:21 +0300

    master

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -1,7 +1,26 @@
 
-window.onload = function()
+ document.getElementById('start').addEventListener('click',function()
 {
     const initPerson = personGenerator.getPerson();
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
-};
+    document.getElementById('surnameOutput').innerText = initPerson.surname;
+    document.getElementById('genderOutput').innerText = initPerson.gender;
+    document.getElementById('birthYearOutput').innerText = initPerson.year;
+    document.getElementById('patronymOutput').innerText = initPerson.patronym;
+    document.getElementById('jobOutput').innerText = initPerson.job;
+    document.getElementById('dateOutput').innerText = initPerson.date;
+    document.getElementById('monthOutput').innerText = initPerson.month;
+})
+
+ document.getElementById('clear').addEventListener('click', function () {
+	document.getElementById('firstNameOutput').innerText = "";
+    document.getElementById('surnameOutput').innerText = "";
+    document.getElementById('genderOutput').innerText = "";
+    document.getElementById('birthYearOutput').innerText = "";
+    document.getElementById('patronymOutput').innerText = "";
+    document.getElementById('jobOutput').innerText = "";
+    document.getElementById('dateOutput').innerText = "";
+    document.getElementById('monthOutput').innerText = "";
+})
+
 
--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -7,7 +7,7 @@ const personGenerator = {
             "id_3": "Кузнецов",
             "id_4": "Васильев",
             "id_5": "Петров",
-            "id_6": "Михайлов",
+            "id_6": "Михайлов",  
             "id_7": "Новиков",
             "id_8": "Федоров",
             "id_9": "Кравцов",
@@ -35,6 +35,102 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
+    
+    patronymMaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Александрич",
+            "id_2": "Максимович",
+            "id_3": "Иванич",
+            "id_4": "Артемович",
+            "id_5": "Дмитриевич",
+            "id_6": "Святославович",
+            "id_7": "Михайлович",
+            "id_8": "Даниилович",
+            "id_9": "Егорович",
+            "id_10": "Андреевич"
+        }
+    }`,
+    jobMaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Юрист",
+            "id_2": "Водитель-экспедитор",
+            "id_3": "Программист",
+            "id_4": "Сантехник",
+            "id_5": "Электрик",
+            "id_6": "Преподаватель",
+            "id_7": "Военнослужащий",
+            "id_8": "Моряк",
+            "id_9": "Маркетолог",
+            "id_10": "Музыкант"
+        }
+    }`,
+
+    firstNameFelJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Ирина",
+            "id_2": "Варвара",
+            "id_3": "Ксения",
+            "id_4": "Елена",
+            "id_5": "Вероника",
+            "id_6": "Людмила",
+            "id_7": "Ева",
+            "id_8": "Юлия",
+            "id_9": "Мария",
+            "id_10": "Екатерина"
+        }
+    }`,
+    patronymFemJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Александровна",
+            "id_2": "Анатольевна",
+            "id_3": "Андреевна",
+            "id_4": "Васильевна",
+            "id_5": "Максимовна",
+            "id_6": "Вячеславовна",
+            "id_7": "Юрьевна",
+            "id_8": "Константиновна",
+            "id_9": "Витальевна",
+            "id_10": "Ивановна"
+        }
+    }`,
+    jobFemJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Cтилист",
+            "id_2": "Парикмахер",
+            "id_3": "Кассир",
+            "id_4": "Бухгалтер",
+            "id_5": "Экономист",
+            "id_6": "Стюардесса",
+            "id_7": "Дизайнер",
+            "id_8": "Менеджер",
+            "id_9": "Финансист",
+            "id_10": "Переводчик"
+        }
+    }`,
+
+    monthJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Января",
+            "id_2": "Февраля",
+            "id_3": "Марта",
+            "id_4": "Апреля",
+            "id_5": "Мая",
+            "id_6": "Июня",
+            "id_7": "Июля",
+            "id_8": "Августа",
+            "id_9": "Сентября",
+            "id_10": "Октября",
+            "id_11": "Ноября",
+            "id_12": "Декабря"
+        }
+    }`,
+
 
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
@@ -47,16 +143,79 @@ const personGenerator = {
         return obj.list[prop];
     },
 
+    
+
     randomFirstName: function() {
-
-        return this.randomValue(this.firstNameMaleJson);
-
+        if(this.person.gender == this.GENDER_MALE){
+            return this.randomValue(this.firstNameMaleJson); 
+        }
+        else{
+            return this.randomValue(this.firstNameFelJson);
+       }
+        
     },
 
 
      randomSurname: function() {
+        
+        if(this.person.gender == this.GENDER_MALE){
+            return this.randomValue(this.surnameJson); 
+        }
+        else{
+            return this.randomValue(this.surnameJson) + 'а';
+       }
 
-        return this.randomValue(this.surnameJson);
+    },
+     
+    randomPatronym: function() {
+
+     if(this.person.gender == this.GENDER_MALE) {
+          return this.randomValue(this.patronymMaleJson); 
+     }
+        else { 
+         return this.randomValue(this.patronymFemJson);
+       }
+
+    },
+    randomJob: function() {
+
+        if(this.person.gender == this.GENDER_MALE) {
+             return this.randomValue(this.jobMaleJson); 
+        }
+           else { 
+            return this.randomValue(this.jobFemJson);
+          }
+   
+       },
+
+
+       randomMonth: function() {
+           
+            return this.randomValue(this.monthJson); 
+       
+    },
+
+    randomGender: function() {
+
+        return Math.round(Math.random()*1) ? this.GENDER_FEMALE : this.GENDER_MALE;
+
+    },
+
+    randomYear: function(){
+        return this.randomIntNumber(2018, 1952) + ' год рождения';
+    },
+
+
+    randomDate: function() {
+        if ((this.person.month == "Апреля") || (this.person.month == "Июня") || (this.person.month == "Сентября") || (this.person.month == "Ноября")) {
+            return this.randomIntNumber(30, 1);
+        }
+         else if (this.person.month == "Февраля") {
+            return  this.randomIntNumber(28, 1);
+         }
+         else ((this.person.month == "Января") || (this.person.month == "Марта") || (this.person.month == "Мая") || (this.person.month == "Июля") || (this.person.month == "Августа") || (this.person.month == "Октября") || (this.person.month == "Декабря")); {
+            return this.randomIntNumber(31, 1);
+        }
 
     },
 
@@ -64,7 +223,21 @@ const personGenerator = {
     getPerson: function () {
         this.person = {};
         // this.person.gender = this.randomGender();
+        this.person.gender = this.randomGender();
         this.person.firstName = this.randomFirstName();
+        this.person.year = this.randomYear();
+        this.person.surname = this.randomSurname();
+        this.person.patronym = this.randomPatronym();
+        this.person.job = this.randomJob();
+        this.person.date = this.randomDate();
+        this.person.month = this.randomMonth();
+          
+
+        
         return this.person;
-    }
+    },
+
+   
 };
+
+ 
